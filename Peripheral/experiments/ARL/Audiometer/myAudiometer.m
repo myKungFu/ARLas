3b8444040fc8713d1fb6bdceee84819c13450c8e
M. Ehsan Khalili <jesuissingsing@gmail.com>
2025-05-22 10:15:23 -0500

    testt

--- Peripheral/experiments/ARL/Audiometer/myAudiometer.m
+++ Peripheral/experiments/ARL/Audiometer/myAudiometer.m
@@ -3,7 +3,7 @@ function [] = myAudiometer(varargin)
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-obj = varargin{1}; % get the arlas object 2025
+obj = varargin{1}; % get the arlas object
 
 % User Modifiable Parameters ----------------------------------------------
 
@@ -19,7 +19,6 @@ obj = varargin{1}; % get the arlas object 2025
     subjID = obj.subjectID;
     arlas_audiometerGDM(subjID,subjAge,obj,probeL,probeR);
 
-    arlas_audiometerGDM(subjID,subjAge,obj,probeL,probeR);
 
 end
 
