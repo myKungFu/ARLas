f05d2cea92ab9dae73d2c9c4ef0dbf4eeea31e5b
M. Ehsan Khalili <jesuissingsing@gmail.com>
2025-05-22 13:47:05 -0500

    Update myAudiometer.m

--- Peripheral/experiments/ARL/Audiometer/myAudiometer.m
+++ Peripheral/experiments/ARL/Audiometer/myAudiometer.m
@@ -18,6 +18,7 @@ obj = varargin{1}; % get the arlas object 2025
 
     subjID = obj.subjectID;
     arlas_audiometerGDM(subjID,subjAge,obj,probeL,probeR);
+    arlas_audiometerGDM(subjID,subjAge,obj,probeL,probeR);
 end
 
 % OLD CODE ----------------------------------------------------------------
